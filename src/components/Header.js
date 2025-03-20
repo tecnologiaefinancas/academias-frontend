738d1edcec28daa095176ace6e09a7715630b593
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-03-20 08:27:22 -0300

    Style new Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,27 +3,32 @@ import React from 'react';
 const Header = () => {
   return (
     <header style={styles.header}>
-      <h1 style={styles.appName}>Academias Buzios e Cabo Frio</h1>
+      <h1 style={styles.appName}>Academias Búzios e Cabo Frio</h1>
     </header>
   );
 };
 
+const link = document.createElement('link');
+link.href = 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
+
 const styles = {
   header: {
     display: 'flex',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
-    padding: '10px 20px',
-    backgroundColor: '#333',
-    color: '#fff'
+    padding: '20px 30px',
+    background: 'linear-gradient(to right,rgb(11, 7, 39), #1c1c1c)', 
+    color: '#f5f5dc', 
+    boxShadow: '0 4px 15px rgba(0, 0, 0, 0.3)', 
+    fontFamily: "'Raleway', sans-serif",
   },
   appName: {
-    padding: '10px',
-    fontSize: '24px',
-    alignItems: 'center',
-    width: '70%', 
-    margin: '0 auto'
-  }
+    fontSize: '32px',
+    fontWeight: 'bold',
+    fontFamily: "'Poppins', serif",
+  },
 };
 
-export default Header;
+export default Header
