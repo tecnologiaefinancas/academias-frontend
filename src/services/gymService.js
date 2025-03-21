fadfe7bfdee12cb6c07298e22948cfabb0176563
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-03-21 15:48:32 -0300

    Adjust getGyms to search in adress and title too.

--- src/services/gymService.js
+++ src/services/gymService.js
@@ -2,20 +2,22 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/api/gyms';
 
-export const getGyms = async (city = "", neighborhood = "") => {
+export const getGyms = async (searchTerm = "") => {
     try {
-        const params = {};
-        if (city) params.city = city;
-        if (neighborhood) params.neighborhood = neighborhood;
 
+        const params = {};
+        if (searchTerm) params.searchTerm = searchTerm.trim(); 
 
         const response = await axios.get(API_URL, { params });
+        
         return response.data; 
     } catch (error) {
-        console.error("Error fetching gyms:", error);
-        throw error;
+
+        console.error("Error to find gyms:", error);
+        throw error; 
     }
 };
+
 
 export const createBook = async (gym) => {
     try {
